c613dcd35feadc8a32da18b701c919923b7c6fbe
Joshua Hayden <joshhayden.business@gmail.com>
2026-04-27 21:30:23 -0400

    Changed button to inherit from box.

--- include/button.hpp
+++ include/button.hpp
@@ -7,12 +7,12 @@
 *   Blink's Thinks is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License v3.0 as published
 *   by the Free Software Foundation.
-*  
+*
 *   Blink's Thinks is distributed in the hope that it will be useful,
 *   but WITHOUT ANY WARRANTY; without even the implied warranty of
 *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *   GNU General Public License for more details.
-*  
+*
 *   You should have received a copy of the GNU General Public License
 *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
@@ -21,7 +21,7 @@
 #pragma once
 
 // Source.
-#include "entity.hpp"
+#include "box.hpp"
 #include "button_traits.hpp"
 #include "text.hpp"
 
@@ -35,7 +35,7 @@ using std::optional;
 namespace engine
 {
 
-class button : public entity
+class button : public box
 {
     public:
         button(
@@ -50,7 +50,7 @@ class button : public entity
 
         void update() override;
         void draw() override;
-    
+
         // Checks if the mouse is within the button's rectangle.
         bool is_hovered();
 
@@ -63,24 +63,12 @@ class button : public entity
 
         text* get_text_obj() { return m_text_obj; }
 
-        Rectangle get_rectangle() { return m_scaled_rec; }
-
-        void set_scale(float scale) { m_scale = scale; }
-
-        float get_scale() { return m_scale; }
-
         void set_sfx_press(Sound sfx_press) { m_sfx_press = sfx_press; }
 
     private:
         // The pointer to the text object of the button. The button handles updating and
         // drawing it's child text within it's own 'update()' and 'draw()' methods.
         text* m_text_obj;
-
-        // The base rectangle without scale applied.
-        Rectangle m_rec;
-
-        // The rectangle with scale applied, used for drawing and collision detection.
-        Rectangle m_scaled_rec;
 
         // What color the text object's text should be when not hovered.
         Color m_default_text_color;
@@ -90,18 +78,6 @@ class button : public entity
 
         // What color the button's background should be when not hovered.
         Color m_default_bg_color;
-
-        // What color the button's background should be in the current frame.
-        Color m_current_bg_color;
-
-        // The color of the outline drawn around the button.
-        Color m_outline_color;
-
-        // The thickness of the outline in pixels.
-        float m_outline_size;
-
-        // What 'm_rectangle' and the text object's 'm_fontSize' are multiplied by.
-        float m_scale;
 
         // The sound effect played when the button is pressed.
         optional<Sound> m_sfx_press;
--- src/button.cpp
+++ src/button.cpp
@@ -7,12 +7,12 @@
 *   Blink's Thinks is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License v3.0 as published
 *   by the Free Software Foundation.
-*  
+*
 *   Blink's Thinks is distributed in the hope that it will be useful,
 *   but WITHOUT ANY WARRANTY; without even the implied warranty of
 *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *   GNU General Public License for more details.
-*  
+*
 *   You should have received a copy of the GNU General Public License
 *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
@@ -37,21 +37,20 @@ button::button(
     Color outline_color,
     float outline_size)
     :
-    entity({rec.x + rec.width / 2.0f, rec.y + rec.height / 2.0f}, layer),
+    box(
+        outline_color,
+        bg_color,
+        {rec.width, rec.height},
+        outline_size,
+        {rec.x + rec.width / 2.0f, rec.y + rec.height / 2.0f},
+        layer
+    ),
     m_text_obj(text_obj),
-    m_rec(rec),
-    m_scaled_rec(rec),
     m_default_text_color(text_obj->get_text_color()),
     m_current_text_color(m_default_text_color),
-    m_default_bg_color(bg_color),
-    m_current_bg_color(m_default_bg_color),
-    m_outline_color(outline_color),
-    m_outline_size(outline_size),
-    m_scale(1.0f)
+    m_default_bg_color(bg_color)
 {
     m_text_obj->set_position(m_position);
-    m_rec.x = m_position.x;
-    m_rec.y = m_position.y;
 }
 
 button::~button()
@@ -66,7 +65,7 @@ button::~button()
 
 bool button::is_hovered()
 {
-    return CheckCollisionPointRec(GetMousePosition(), m_scaled_rec);
+    return CheckCollisionPointRec(GetMousePosition(), get_rectangle());
 }
 
 bool button::is_pressed()
@@ -75,50 +74,42 @@ bool button::is_pressed()
 }
 
 void button::update()
-{ 
-    entity::update();
-
+{
     for (auto& trait : m_traits) {
         trait->update(*this);
     }
 
-    m_scaled_rec = {
-        m_position.x - ((m_rec.width * m_scale) / 2.0f),
-        m_position.y - ((m_rec.height * m_scale) / 2.0f),
-        m_rec.width * m_scale,
-        m_rec.height * m_scale
-    };
+    box::update();
 
-    m_text_obj->set_scale(m_scale);
-    m_text_obj->set_position(m_position);
+    set_fill_color(
+        (is_hovered() && m_default_bg_color.a != 0)
+            ? brighten_color(m_default_bg_color)
+            : m_default_bg_color
+    );
 
     m_current_text_color = is_hovered()
         ? brighten_color(m_default_text_color)
         : m_default_text_color;
 
-    m_current_bg_color = (is_hovered() && m_default_bg_color.a != 0)
-        ? brighten_color(m_default_bg_color)
-        : m_default_bg_color;
-
     if (is_pressed() && m_sfx_press.has_value()) {
         PlaySound(*m_sfx_press);
     }
 
-    m_text_obj->set_text_color(m_current_text_color); 
-
-    m_text_obj->update(); 
+    m_text_obj->set_text_color(m_current_text_color);
+    m_text_obj->set_scale(get_scale());
+    m_text_obj->set_position(m_position);
+    m_text_obj->update();
 }
 
 void button::draw()
 {
-    DrawRectangleRec(m_scaled_rec, m_current_bg_color);
-    DrawRectangleLinesEx(m_scaled_rec, m_outline_size, m_outline_color);
+    box::draw();
     m_text_obj->draw();
 }
 
 Color button::brighten_color(Color color)
 {
-    return { 
+    return {
         static_cast<unsigned char>(fmin(color.r * m_brighten_factor, 255)),
         static_cast<unsigned char>(fmin(color.g * m_brighten_factor, 255)),
         static_cast<unsigned char>(fmin(color.b * m_brighten_factor, 255)),
